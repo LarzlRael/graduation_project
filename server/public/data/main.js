d728a789cadc2584e0df583ad7612a768748e89b
Larz <rael.thasss@gmail.com>
2021-09-13 15:33:59 -0400

    changing to other layer map

--- server/public/data/main.js
+++ server/public/data/main.js
@@ -1,8 +1,9 @@
 const form = document.querySelector('#form');
 const date = document.querySelector('#calendario');
 
-form.addEventListener('submit', (e) => {
+/* form.addEventListener('submit', (e) => {
   e.preventDefault();
   console.log(date.value);
   pintarMapa(date.value);
 });
+ */
